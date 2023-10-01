44e1dee2e4b7a08a22a08aa40cc3246a11e23bdb
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2023-10-01 08:24:46 +0100

    Initial Commit

---
+++ HotelCLIProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HotelCLIProject
+//
+//  Created by Thamsanqa Mbano on 01/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
