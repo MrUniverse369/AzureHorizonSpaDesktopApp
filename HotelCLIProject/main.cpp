7ba6025a54e31a157b9a2d54a9508ee408fd45bd
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2023-10-02 18:11:02 +0100

    loginformstart

--- HotelCLIProject/main.cpp
+++ HotelCLIProject/main.cpp
@@ -1,14 +1,280 @@
-//
-//  main.cpp
-//  HotelCLIProject
-//
-//  Created by Thamsanqa Mbano on 01/10/2023.
-//
+// hotelAssignment.cpp : Defines the entry point for the console application.
+///*purpose of this assinment is to work on my c++*/
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cstdlib>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int dog = 100;
+std::vector<std::string> hotelRooms(100, "EMPTY");
+std::vector<std::string>::const_iterator conIter;
+std::vector<std::string>::iterator iter;
+std::ofstream hotelFiles;
+std::ifstream hotelFilesRead;
+
+char userInput;
+void Login();
+void Book();
+void MenueSystem();
+void ViewAllRooms();
+void AddCustomersToRooms(std::string customerName, int roomNumber);
+void DisplayEmptyRooms();
+void DeleteCustomerFromRoom();
+void FindRoomFromCustomerName();
+void StoreProgramDataToFile();
+void LoadProgamDataFromFile();
+void SortRoomByAlphabet();
+void ExitToMenue();
+void ER();
+int EXIT = 0;
+int counter = 0;
+char filename[50];
+
+
+
+int main()
+{
+    std::cout<<"Welcome to Azure HorizonInn \n";
+    std::cout <<"1:Login \n2:View Rooms \n3:Book \n\n";
+    std::cin>>userInput;
+    switch (userInput) {
+        case 1:
+            Login();
+            break;
+        case 2:
+            ViewAllRooms();
+            break;
+        case 3:
+            Book();
+            break;
+        default:
+            break;
+    }
+
+    MenueSystem();
     return 0;
 }
+void MenueSystem() {
+    std::string customerName, tempVer;
+    int roomNumber;
+
+    std::cout <<"V: View All Rooms\nA: AddCustomersToRooms\nE: DisplayEmptyRooms\nD: DeleteCustomerFromRoom();\nF: FindRoomFromCustomerName\nS: StoreProgramDataToFile\nL: LoadProgamDataFromFile\nO: SortRoomByAlphabet "<< std::endl;
+    std::cin >> userInput;
+
+    switch (toupper(userInput))
+    {
+    case 'V':
+        std::cout << "this is a llist of who is staying in every room" << std::endl;
+        ViewAllRooms();
+        break;
+    case 'A':
+        std::cout << "You choose Add customer to room"<<std::endl;
+        std::cout << "Enter the customer name" << std::endl;
+        std::cin >> tempVer;
+        customerName = tempVer;
+        std::cout << "Enter the customers roomNumber";
+        std::cin >> roomNumber;
+        AddCustomersToRooms(customerName , roomNumber-1);
+        break;
+    case 'E':
+        std::cout << "display empty rooms"<<std::endl;
+        DisplayEmptyRooms();
+        break;
+    case 'D':
+        std::cout << "Delete customer from room" << std::endl;
+        DeleteCustomerFromRoom();
+        break;
+    case 'F':
+        std::cout << "Find room from customer name " << std::endl;
+        FindRoomFromCustomerName();
+        break;
+    case 'S':
+        std::cout << "Store program DataFile" << std::endl;
+        StoreProgramDataToFile();
+        break;
+    case 'L':
+        std::cout << "Load Datafile" << std::endl;
+        
+        
+        LoadProgamDataFromFile();
+        break;
+    case 'O':
+        std::cout << "sort customer names alphabetially" << std::endl;
+        SortRoomByAlphabet();
+        break;
+
+    default:
+        break;
+    }
+}
+void Book(){
+    }
+void Login(){
+    
+}
+void ViewAllRooms() {
+    
+     counter = 1;
+     std::cout << "Customer name    room number" << std::endl;
+    for (conIter = hotelRooms.begin(); conIter <= hotelRooms.end() - 1;  conIter++,counter++)
+    {
+        std::cout << *conIter<<"                 "<<counter<< std::endl;
+
+    }
+    ExitToMenue();
+}
+void AddCustomersToRooms(std::string customerName, int roomNumber) {
+
+    std::cout << "Type in the name of the customer you want to add to the hotel admin" << std::endl;
+    hotelRooms[roomNumber] = customerName;
+    ExitToMenue();
+
+}
+void DisplayEmptyRooms()
+{
+    
+    iter = hotelRooms.begin();
+    counter = 1;
+    for (iter = hotelRooms.begin(); iter <= hotelRooms.end() - 1; iter++ ,counter++)
+    {
+        if (*iter == "EMPTY")
+        {
+            std::cout << *iter <<"  "<<counter<< std::endl;
+        }
+
+    }
+    ExitToMenue();
+    
+    
+}
+void DeleteCustomerFromRoom()
+{
+    std::string customerName;
+    std::cin >> customerName;
+
+    //this algorithm finds the customer by namee and sets the iter to refer to the customers room
+
+    iter = find(hotelRooms.begin(), hotelRooms.end(), customerName);
+    counter = std::distance(hotelRooms.begin(), iter);
+
+    /*this code checks to see if the customer we searched for exist if they
+    do it runs the if statment whi Tch deletes our customer and if the customer
+    name  can't be found it returns a error message */
+
+    if (iter != hotelRooms.end())
+    {
+        std::cout <<*iter<< " Has been removed from the room it is now empty " << "\nhis room number was " << counter + 1 << std::endl;
+        hotelRooms.erase(iter);
+        //hotelRooms[counter] = "EMPTY";
+    }
+    else
+    {
+        std::cout << "The customer is not staying in the hotel" << std::endl;
+    }
+    
+    
+    ExitToMenue();
+}
+void FindRoomFromCustomerName()
+{
+    std::string customerName;
+    std::cin >> customerName;
+    iter = find(hotelRooms.begin(), hotelRooms.end(), customerName);
+    counter = std::distance(hotelRooms.begin(), iter);
+
+    if (iter != hotelRooms.end())
+    {
+        std::cout << "The customer is staying in the hotel\nHis name is"<<*iter<<"\nhis room number is "<<counter+1<<std::endl;
+    }
+    else
+    {
+        std::cout << "The customer is not staying in the hotel" << std::endl;
+    }
+    
+    
+    ExitToMenue();
+}
+void StoreProgramDataToFile()
+{
+    std::string userInput;
+
+    std::cout << "Enter file name and press enter to save"<<std::endl;
+    std::cin >> userInput;
+    hotelFiles.open(userInput);
+    for (iter = hotelRooms.begin(); iter <= hotelRooms.end() - 1; iter++)
+    {
+
+        hotelFiles << *iter<<std::endl;
+    }
+    hotelFiles.close();
+    ExitToMenue();
+}
+void LoadProgamDataFromFile()
+{
+    std::cin.ignore();
+    std::cin.getline(filename, 50);
+    hotelFilesRead.open(filename);
+    if (!hotelFilesRead.is_open())
+    {
+        //exit(EXIT_FAILURE);
+        std::cout << "FILE NOT FOUND"<< std::endl;
+    }
+    else
+    {
+        std::cout << "FILE  FOUND" << std::endl;
+    }
+
+    char word[50];
+    hotelFilesRead >> word;
+    int i = 0;
+    while (hotelFilesRead.good())
+    {
+        std::cout << word << " \n";
+        hotelFilesRead >> word;
+        iter = hotelRooms.begin();
+        hotelRooms[i] = word;
+        ++i;
+    }
+    hotelFilesRead.close();
+    ExitToMenue();
+}
+void SortRoomByAlphabet()
+{
+    std::sort(hotelRooms.begin(),hotelRooms.end());
+
+    ExitToMenue();
+
+}
+void ExitToMenue() {
+
+    std::cout << "Enter one to go back to main menue " << std::endl;
+        std::cin >> EXIT;
+    if (EXIT == 1)
+    {
+        MenueSystem();
+    }
+
+}
+
+class HotelCustomers {
+public:
+    std::string CustomerName;
+    int roomNumber;
+    void storeVals(std::string customerName, int rmNumber) {
+        std::cout << "please type in customer name followed by room number" << std::endl;
+        std::cin >> customerName;
+        std::cin >> roomNumber;
+    }
+private:
+
+
+
+};
+
+
